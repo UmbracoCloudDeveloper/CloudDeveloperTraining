4e795273750aca356b52eee04eaf70ff4a27a610
Rasmus Burich <rbu@umbraco.dk>
2025-11-21 08:45:05 +0100

    Added Send email workflows

---
+++ src/UmbracoProject/Views/Partials/Forms/Emails/PonyBooking.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace UmbracoProject.Views.Partials.Forms.Emails
+{
+    public class PonyBookingModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
